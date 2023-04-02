ec4aa630e95eec26b09d95f5450c316f0bf8fd5f
mohanad mostafa <mohand.mostafa9811@gmail.com>
2023-04-02 12:45:21 +0200

    last change

--- app.js
+++ app.js
@@ -37,47 +37,146 @@ app.use(cors())
 //       });
 // }
 // );
-
-app.get('store_key/ios', function(req, res) {
-    console.log('req.body', req.body)
-    res.status(200);
-         res.json(
-            [
-                {
-                  "ulid": "J019F8GX4KKGQJG14DH7AP2G23", 
-                  "key_id": "QR9L9L7VK2",
-                  "applications": 
-                    [123,1221] 
-                }
-              ]
-         );
-}
-);
 app.post('/store_key/ios', function(req, res) {
     console.log('req.body', req.body)
-    res.status(200);
-         res.json(
-            {
-                "ulid": "5252",
-                "store_ids": [{"bundle_id": "a", ios_app_id:1},
-                {"bundle_id": "b", ios_app_id:8},
-                {"bundle_id": "c", ios_app_id:2},
-                {"bundle_id": "d", ios_app_id:3},
-                {"bundle_id": "e", ios_app_id:4},
-                {"bundle_id": "f", ios_app_id:5},
-                {"bundle_id": "g", ios_app_id:6},
-                {"bundle_id": "h", ios_app_id:7}]
-              }
-         );
+    if(req.body.status ===   500) res.sendStatus(200);
+    else  setTimeout(()=>{
+        res.json({
+            "ulid": "12",
+            "store_ids": [
+                {"bundle_id": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", ios_app_id:1},
+                {"bundle_id": "b", ios_app_id:2},
+                {"bundle_id": "c", ios_app_id:3},
+                {"bundle_id": "d", ios_app_id:4},
+                {"bundle_id": "e", ios_app_id:5},
+                {"bundle_id": "f", ios_app_id:6},
+                {"bundle_id": "g", ios_app_id:7},
+                {"bundle_id": "h", ios_app_id:8},
+                {"bundle_id": "i", ios_app_id:9},
+                {"bundle_id": "j", ios_app_id:10},
+                {"bundle_id": "k", ios_app_id:11},
+                {"bundle_id": "l", ios_app_id:12},
+
+            ]
+          });
+    },2000)
 }
 );
+
+// app.get('/store_key/ios', function(req, res) {
+//     console.log('req.body', req.body)
+//     res.status(200);
+//     setTimeout(()=>{
+//         res.json(
+//             [ ]
+//          );
+//     },2000)
+       
+// }
+// );
+
+app.get('/store_key/ios', function(req, res) {
+    console.log('req.body', req.body)
+    res.status(200);
+    setTimeout(()=>{
+            res.json(
+                [
+                    {
+                      "ulid": "J019F8GX4KKGQJG14DH7AP2G23", 
+                      "key_id": "QR9L9L7VK2",
+                      "applications": 
+                        [123,1221] 
+                    }
+                  ]
+             );
+        },2000)
+}
+);
+// app.get('/store_key/ios', function(req, res) {
+//     console.log('req.body', req.body)
+//     res.status(200);
+//          res.json(
+//             []
+//          );
+// }
+// );
+// app.post('/store_key/ios', function(req, res) {
+//     console.log('req.body', req.body)
+//     res.status(200);
+//          res.json(
+//             {
+//                 "ulid": "5252",
+//                 "store_ids": [{"bundle_id": "a", ios_app_id:1},
+//                 {"bundle_id": "b", ios_app_id:8},
+//                 {"bundle_id": "c", ios_app_id:2},
+//                 {"bundle_id": "d", ios_app_id:3},
+//                 {"bundle_id": "e", ios_app_id:4},
+//                 {"bundle_id": "f", ios_app_id:5},
+//                 {"bundle_id": "g", ios_app_id:6},
+//                 {"bundle_id": "h", ios_app_id:7}]
+//               }
+//          );
+// }
+// );
+
+// app.post('/store_key/ios', function(req, res) {
+//     console.log('req.body', req.body)
+//     res.status(200);
+//          res.json(
+//             {
+//                 "ulid": "5252",
+//                 "store_ids": []
+//               }
+//          );
+// }
+// );
 
 app.post('/store_integration/ios', function(req, res) {
     console.log('req.body', req.body)
     res.status(200);
-    res.send()
+    setTimeout(()=>{
+        res.send()
+    },2000)
     }
 );
+// app.get('/store_integration/ios', function(req, res) {
+//     console.log('req.body', req.body)
+//     res.status(200);
+//     res.send([
+//         {
+//           ulid: "",           //id for store integration (to be used if store integration is updated or deleted)
+//           bundle_id: "",		//for both android and iOS
+//           reachable: true,	//true: "Setup completed", false: "Unreachable"
+//         }
+//       ])
+//     }
+// );
+app.get('/store_integration/ios', function(req, res) {
+    console.log('req.body', req.body)
+    res.status(200);
+    res.send([])
+    }
+);
+
+
+
+app.get('/store_key/ios/list_store_bundle_ids', function(req, res) {
+    console.log('req.body', req.body)
+    console.log('req.query', req.query)
+    res.status(200);
+    setTimeout(()=>{
+    res.json({"store_ids": [{"bundle_id": "a", ios_app_id:1}]})
+    },2000);
+}
+)
+;
+
+// {
+//     error: "Invalid authentication, we are unable to fetch the app bundle IDs from your App store connect account Please, try again"
+//   }
+//         "store_ids": []
+// "store_ids": [{"bundle_id": "a", ios_app_id:1}]
+
 // run server on port 3000
 app.listen(port, function() {
     console.log('Server running on port ' + port);
